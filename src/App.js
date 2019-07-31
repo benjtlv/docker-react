68ea1016e7c939e50a5cb423bddcbcaa34fc1b23
Benjamin Elkrieff <benjamin.elkrieff@armis.com>
2019-07-31 18:54:16 +0300

    feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Shalom mimi
+          Shalom mimi feature
         </a>
       </header>
     </div>
